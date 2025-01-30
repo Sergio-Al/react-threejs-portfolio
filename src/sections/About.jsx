34dd672a90df4d0b82cb92386a15d01c5f3d85ca
Sergio-Al <sergio.alejandro.ml900@gmail.com>
2025-01-30 19:57:20 -0400

    Feat. About section completed

---
+++ src/sections/About.jsx
@@ -0,0 +1,129 @@
+import React, { useState } from "react";
+import Globe from "react-globe.gl";
+import Button from "../components/Button";
+
+const About = () => {
+  const myEmail = "sergio.alejandro.ml900@gmail.com";
+  const [hasCopied, setHasCopied] = useState(false);
+
+  const handleCopy = () => {
+    navigator.clipboard.writeText(myEmail);
+
+    setHasCopied(true);
+
+    setTimeout(() => {
+      setHasCopied(false);
+    }, 2000);
+  };
+  return (
+    <section className="c-space my-20">
+      <div className="grid xl:grid-cols-3 xl:grid-rows-6 md:grid-cols-2 grid-cols-1 gap-5 h-full">
+        <div className="col-span-1 xl:row-span-3">
+          <div className="grid-container">
+            <img
+              src="/assets/grid1.png"
+              alt="grid-1"
+              className="w-full sm:h-[276px] h-fit object-contain"
+            />
+            <div>
+              <p className="grid-headtext">Hi I'm Sergio</p>
+              <p className="grid-subtext">
+                With 5 years of experience, I have mastered my backend +
+                frontend skills to another level, I like to build 3d websites as
+                well. :){" "}
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="col-span-1 xl:row-span-3">
+          <div className="grid-container">
+            <img
+              src="/assets/grid2.png"
+              alt="grid-2"
+              className="w-full sm:h-[276px] h-fit object-contain"
+            />
+            <div>
+              <p className="grid-headtext">What I do</p>
+              <p className="grid-subtext">
+                I have been working on various projects, from building software
+                to AI and products.{" "}
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="col-span-1 xl:row-span-4">
+          <div className="grid-container">
+            <div className="rounded-3xl w-full sm:h-[326px] h-fit flex justify-center items-center">
+              <Globe
+                height={326}
+                width={326}
+                backgroundColor="rgba(0,0,0,0)"
+                backgroundImageOpacity={0.5}
+                showAtmosphere
+                showGraticules
+                globeImageUrl="//unpkg.com/three-globe/example/img/earth-night.jpg"
+                bumpImageUrl="//unpkg.com/three-globe/example/img/earth-topology.png"
+                labelsData={[
+                  {
+                    lat: 40,
+                    lng: -100,
+                    text: "Hello",
+                    color: "white",
+                    size: 20,
+                  },
+                ]}
+              />
+            </div>
+            <div>
+              <p className="grid-headtext">Where I work</p>
+              <p className="grid-subtext">
+                I am currently working at a company called XYZ, where I am
+                building software and AI products.{" "}
+              </p>
+              <Button name="Contact me" isBeam containerClass="w-full mt-10" />
+            </div>
+          </div>
+        </div>
+        <div className="xl:col-span-2 xl:row-span-3">
+          <div className="grid-container">
+            <img
+              src="/assets/grid3.png"
+              alt="3"
+              className="w-full sm:h-[266px] h-fit object-contain"
+            />
+            <div>
+              <p className="grid-headtext">My Skills</p>
+              <p className="grid-subtext">
+                I have mastered my skills in building software, AI, and
+                products.{" "}
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="xl:col-span-1 xl:row-span-2">
+          <div className="grid-container">
+            <img
+              src="/assets/grid4.png"
+              alt="4"
+              className="w-full md:h-[126px] sm:h-[276px] h-fit object-cover sm:object-top"
+            />
+            <div className="space-y-2">
+              <p className="grid-subtext text-center">Contact Me</p>
+              <p className="copy-container" onClick={handleCopy}>
+                <img
+                  src={hasCopied ? "/assets/tick.svg" : "/assets/copy.svg"}
+                  alt="copy"
+                />
+                <p className="lg:text-xl md:text-sm font-medium text-gray_gradient text-white">
+                  {myEmail}
+                </p>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default About;
