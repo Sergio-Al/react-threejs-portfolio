d39bcdcdecd15ed2bf959d630a49c145222c88e4
Sergio-Al <sergio.alejandro.ml900@gmail.com>
2025-01-30 20:49:19 -0400

    Feat. About section

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Navbar from "./sections/Navbar";
 import Hero from "./sections/Hero";
 import About from "./sections/About";
+import Projects from "./sections/Projects";
 
 const App = () => {
   return (
@@ -10,9 +11,9 @@ const App = () => {
       <Navbar />
       <Hero />
       <About />
+      <Projects />
     </main>
   );
 };
 
 export default App;
- 
